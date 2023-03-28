d2ac0799b107a93b7974d88bb6082fd3185e0a8b
dev <66136072+suyash-web@users.noreply.github.com>
2023-03-28 21:15:35 +0530

    Add files via upload

---
+++ Home/urls.py
@@ -0,0 +1,27 @@
+"""Home URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+admin.site.site_header = "Suyash Ice Creams"
+admin.site.site_title = "Suyash Ice Creams Admin Portal"
+admin.site.index_title = "Welcome to Suyash Ice Creams"
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("IceCreamShop.urls")),
+    path("about/", include("IceCreamShop.urls")),
+    path("contact/", include("IceCreamShop.urls"))
+]
